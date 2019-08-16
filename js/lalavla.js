793f0a1461dc5c981f9c0da56dc5f0b58d70d083
yelee0603 <52442916+yelee0603@users.noreply.github.com>
2019-08-16 15:20:10 +0900

    Update lalavla.js

--- js/lalavla.js
+++ js/lalavla.js
@@ -251,12 +251,13 @@ $(function(){
         else if($(window).width()>767){
             $('.top-area .menu-btn').addClass('pc')
             $('.top-area .menu-btn').removeClass('mo')
-            $('.depth1 a').click(function(){
-                $('.sub').removeClass('active');
-                $(this).next().addClass('active');
+            $('.depth1 a').click(function(e){
+                e.preventDefault();
+                $('.sub').toggleClass('active');
+                // $(this).next().addClass('active');
             })        
             //pc용
-            $('.menu-btn.pc').click(function(){
+            $('.menu-btn.pc').click(function(e){
                 $('.menu-pc').show();//피시 메뉴
                 $('.menu-pc .depth1').toggleClass('active');
                 $('.menu-popup').hide();//모바일 팝업
